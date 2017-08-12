8aaf48d8e0ac3dcb07051658de795fad8ab0d6e2
Nautilus A. Jungle <iamnautilusjungle@gmail.com>
2017-08-12 22:38:24 +0200

    World load correctly from .txt

--- src/stabletable/Main/states/GameState.java
+++ src/stabletable/Main/states/GameState.java
@@ -4,23 +4,28 @@ import java.awt.Graphics;
 
 import stabletable.Main.Game;
 import stabletable.Main.entities.creatures.Player;
+import stabletable.Main.worlds.World;
 
 public class GameState extends State{
 
 	private Player player;
+	private World world;
 	
 	public GameState(Game game) {
 		super(game);
 		player = new Player(game, 100,100);
+		world = new World("res/worlds/world1.txt");
 	}
 	
 	@Override
 	public void tick() {
+		world.tick();
 		player.tick();
 	}
 
 	@Override
 	public void render(Graphics g) {
+		world.render(g);
 		player.render(g);
 	}
 
---
+++ src/stabletable/Main/worlds/World.java
@@ -0,0 +1,52 @@
+package stabletable.Main.worlds;
+
+import java.awt.Graphics;
+
+import stabletable.Main.tiles.Tile;
+import stabletable.Main.utils.Utils;
+
+public class World {
+
+	private int width, height, spawnX, spawnY;
+	private int[][] tiles;
+	
+	public World(String path) {
+		loadWorld(path);
+	}
+	
+	public void tick() {
+		
+	}
+	
+	public void render(Graphics g) {
+		for(int y = 0;y < height;y++) {
+			for(int x = 0;x < width;x++) {
+				getTile(x,y).render(g, x*Tile.TILEWIDTH, y*Tile.TILEHEIGHT);
+			}
+		}
+	}
+	
+	public Tile getTile(int x, int y) {
+		Tile t = Tile.tiles[tiles[x][y]];
+		if(t == null)
+			return Tile.dirtTile;
+		return t;
+	}
+	
+	private void loadWorld(String path) {
+		String file = Utils.loadFileAsString(path);
+		String[] tokens = file.split("\\s+");
+		width = Utils.parseInt(tokens[0]);
+		height = Utils.parseInt(tokens[1]);
+		spawnX = Utils.parseInt(tokens[2]);
+		spawnY = Utils.parseInt(tokens[3]);
+		
+		tiles = new int[width][height];
+		for(int y = 0;y<height;y++) {
+			for(int x=0;x<width;x++) {
+				tiles[x][y] = Utils.parseInt(tokens[(x+y*width)+4]);
+			}
+		}
+	}
+	
+}
