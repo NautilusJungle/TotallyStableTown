562bdf217b3fc3223ebd7cee9a322f30c6abafef
Nautilus A. Jungle <iamnautilusjungle@gmail.com>
2017-08-12 20:27:45 +0200

    Contrôles en ZQSD

--- src/stabletable/Main/Game.java
+++ src/stabletable/Main/Game.java
@@ -30,6 +30,10 @@ public class Game implements Runnable {
 	// Input
 	private KeyManager keyManager;
 	
+	public KeyManager getKeyManager() {
+		return keyManager;
+	}
+
 	public Game(String title, int width, int height) {
 		this.width = width;
 		this.height = height;
@@ -74,6 +78,8 @@ public class Game implements Runnable {
 	
 	
 	private void tick() {
+		keyManager.tick();
+		
 		if(State.getState() != null)
 			State.getState().tick();
 	}
--- src/stabletable/Main/entities/creatures/Player.java
+++ src/stabletable/Main/entities/creatures/Player.java
@@ -2,17 +2,30 @@ package stabletable.Main.entities.creatures;
 
 import java.awt.Graphics;
 
+import stabletable.Main.Game;
 import stabletable.Main.gfx.Assets;
+import stabletable.Main.input.KeyManager;
 
 public class Player extends Creature {
 
-	public Player(float x, float y) {
-		super(x, y);
+	private Game game;
+	
+	public Player(Game game, float x, float y) {
+		super(x, y, Creature.DEFAULT_CREATURE_WIDTH, DEFAULT_CREATURE_HEIGHT);
+		this.game = game;
 	}
 
 	@Override
 	public void tick() {
-
+		KeyManager k = game.getKeyManager();
+		if(k.up)
+			y-= 3;
+		if(k.down)
+			y+= 3;
+		if(k.left)
+			x-= 3;
+		if(k.right)
+			x+= 3;
 	}
 
 	@Override
